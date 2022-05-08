d22939c500e5953d004cc89840df3922661de1d3
denis <denisdenis9331@gmail.com>
2022-05-09 00:56:00 +0500

    add structure

---
+++ src/main/java/com/tradealertbot/tradealert/TradealertApplication.java
@@ -0,0 +1,13 @@
+package com.tradealertbot.tradealert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradealertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradealertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradealertbot/tradealert/TradealertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradealertbot.tradealert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradealertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
